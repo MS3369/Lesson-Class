11051eb6e334a895ea0f0936777595076b99d05d
Shahz <ms3369@live.mdx.ac.uk>
2023-03-15 20:39:36 +0400

    first

---
+++ lessons.js
@@ -0,0 +1,62 @@
+let products = [
+    {
+        "id" : 101,
+        "image" :'image.jpg',
+        "subject" : 'Laser Pointer',
+        "location": 'Sweden',
+        "price" : 220,
+        "detail": 'this is a Laser pointer from Sweden',
+        "availableInventory": 8,
+        "rating": 2 
+    },
+
+    {
+    
+        "id" : 202,
+        "image" :'image.jpg',
+        "subject" : 'Laser Pointer',
+        "location": 'Sweden',
+        "price" : 220,
+        "detail": 'this is a Laser pointer from Sweden',
+        "availableInventory": 8,
+        "rating": 3
+    },
+
+    {
+        
+        "id" : 303,
+        "image" :'image.jpg',
+        "subject" : 'Laser Pointer',
+        "location": 'Sweden',
+        "price" : 220,
+        "detail": 'this is a Laser pointer from Sweden',
+        "availableInventory": 8,
+        "rating": 4
+    },
+
+    {
+        
+        "id" : '404',
+        "image" :'image.jpg',
+        "subject" : 'Laser Pointer',
+        "location": 'Sweden',
+        "price" : 220,
+        "detail": 'this is a Laser pointer from Sweden',
+        "availableInventory": 8,
+        "rating": 2
+    },
+
+    {
+        
+        "id" : 505,
+        "image" :'image.jpg',
+        "subject" : 'Laser Pointer',
+        "location": 'Sweden',
+        "price" : 220,
+        "detail": 'this is a Laser pointer from Sweden',
+        "availableInventory": 8,
+        "rating": 5
+    }
+
+            
+]
