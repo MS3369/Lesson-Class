bd5603465976491aa2b3bdac44b7f29f1a5c9b5c
Shahz <ms3369@live.mdx.ac.uk>
2023-03-15 23:06:36 +0400

    second

--- lessons.js
+++ lessons.js
@@ -1,62 +1,118 @@
 let products = [
     {
-        "id" : 101,
-        "image" :'image.jpg',
-        "subject" : 'Laser Pointer',
-        "location": 'Sweden',
+        "id" : 001,
+        "image" :'math.jpg',
+        "subject" : 'Mathematics',
+        "location": 'Townhall',
         "price" : 220,
-        "detail": 'this is a Laser pointer from Sweden',
-        "availableInventory": 8,
+        "detail": 'this is a beginner maths class',
+        "availableInventory": 5,
         "rating": 2 
     },
 
     {
     
-        "id" : 202,
-        "image" :'image.jpg',
-        "subject" : 'Laser Pointer',
-        "location": 'Sweden',
-        "price" : 220,
-        "detail": 'this is a Laser pointer from Sweden',
-        "availableInventory": 8,
+        "id" : 101,
+        "image" :'science.jpg',
+        "subject" : 'Science',
+        "location": 'City Centre',
+        "price" : 350,
+        "detail": 'this is a beginner science class',
+        "availableInventory": 5,
         "rating": 3
     },
 
     {
         
-        "id" : 303,
-        "image" :'image.jpg',
-        "subject" : 'Laser Pointer',
-        "location": 'Sweden',
-        "price" : 220,
-        "detail": 'this is a Laser pointer from Sweden',
-        "availableInventory": 8,
+        "id" : 202,
+        "image" :'history.jpg',
+        "subject" : 'History',
+        "location": 'Outskirts city',
+        "price" : 290,
+        "detail": 'this is a beginner history class',
+        "availableInventory": 5,
         "rating": 4
     },
 
     {
         
-        "id" : '404',
-        "image" :'image.jpg',
-        "subject" : 'Laser Pointer',
-        "location": 'Sweden',
-        "price" : 220,
-        "detail": 'this is a Laser pointer from Sweden',
-        "availableInventory": 8,
+        "id" : 303,
+        "image" :'english.png',
+        "subject" : 'English',
+        "location": 'Downtown',
+        "price" : 240,
+        "detail": 'This is an English class for beginners',
+        "availableInventory": 5,
         "rating": 2
     },
 
     {
         
-        "id" : 505,
-        "image" :'image.jpg',
-        "subject" : 'Laser Pointer',
-        "location": 'Sweden',
-        "price" : 220,
-        "detail": 'this is a Laser pointer from Sweden',
-        "availableInventory": 8,
+        "id" : 404,
+        "image" :'art.jpg',
+        "subject" : 'Arts & Drawing',
+        "location": 'Market Area',
+        "price" : 310,
+        "detail": 'This is an Arts & Drawing class',
+        "availableInventory": 5,
         "rating": 5
-    }
+    },
 
-            
+    {
+        
+        "id" : 505,
+        "image" :'computer.jpg',
+        "subject" : 'Computing',
+        "location": 'Beach Area',
+        "price" : 460,
+        "detail": 'Computing Introduction course',
+        "availableInventory": 5,
+        "rating": 3.5
+    },
+    
+    {
+        
+        "id" : 606,
+        "image" :'social.png',
+        "subject" : 'Social Studies',
+        "location": 'Townhall',
+        "price" : 190,
+        "detail": 'Social studies beginner lesson',
+        "availableInventory": 5,
+        "rating": 4
+    },
+
+    {
+        
+        "id" : 707,
+        "image" :'chemistry.jpg',
+        "subject" : 'Chemistry',
+        "location": 'Outskirts city',
+        "price" : 310,
+        "detail": 'Arts & Drawing class',
+        "availableInventory": 5,
+        "rating": 5
+    }, 
+    {
+        
+        "id" : 808,
+        "image" :'baking.jpg',
+        "subject" : 'Baking',
+        "location": 'Market Area',
+        "price" : 250,
+        "detail": 'Baking class for beginners',
+        "availableInventory": 5,
+        "rating": 4
+    },
+    {
+        
+        "id" : 909,
+        "image" :'french.jpg',
+        "subject" : 'French',
+        "location": 'City Centre',
+        "price" : 310,
+        "detail": 'French learning lesson for beginners',
+        "availableInventory": 5,
+        "rating": 2
+    },
 ]
